f71f0286b9b49525046499744662bbc11974ace1
FRNBDN <francesco.badini94@gmail.com>
2023-06-02 22:25:29 +0900

    collect sales data of last 5 markets in each col

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -80,6 +82,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for col in range(1, 7):
+        column = sales.col_values(col)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """Run all program functions"""
     data = get_sales_data()
@@ -90,4 +108,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
